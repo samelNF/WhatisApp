3f03ebb7fcff800e0d0a1dfa1cac6cc81bfb5e31
samelNF <nobrsamuel@gmail.com>
2026-09-09 21:56:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ function mostrarSenha() {
         senha.type = "text";
     } else {
         senha.type = "password";
-    }
+    };
 }
 
 function criarConta() {
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
     const usuario = document.getElementById("usuario").value;
+    const criarConta = document.getElementById("criar-conta");
 
     if (email === "" || senha === "" || usuario === "") {
         alert("Por favor, preencha todos os campos.");
         return;
-    }
+    };
     if (!email.includes("@") || !email.includes(".")) {
         alert("Digite um email válido.");
         return;
-    }
+    };
     if (senha.length < 8 || senha.length > 12) {
         alert("A senha tem que ter entre 8 e 12 caracteres.");
         return;
-    }
+    };
     
-    console.log("pronto")
+    console.log("pronto");
    
     const conta = {
         email: email,
@@ -34,10 +35,27 @@ function criarConta() {
         usuario: usuario
     };
 
-    localStorage.setItem("conta", JSON.stringify(conta));
-    email.value = "";
-    usuario.value = "";
-    senha.value = "";
+    localStorage.setItem("conta", JSON.stringify({
+    email: email,
+    usuario: usuario,
+    senha: senha
+    }));
 
-    console.log("pronto");
+    console.log("Conta criada!");
+
+    criarConta.style.display = "none";
+    
+}
+function pegarConta() {
+    const dados = localStorage.getItem("conta");
+
+    if (dados === null) {
+        console.log("Nenhuma conta encontrada.");
+        return;
+    }
+
+    const conta = JSON.parse(dados);
+
+    console.log("Email:", conta.email);
+    console.log("Usuário:", conta.usuario);
 }
