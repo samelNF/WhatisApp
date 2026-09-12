327ef5e69aa1dde61aeea4b003fcc7fa687f827d
samelNF <nobrsamuel@gmail.com>
2026-09-11 22:06:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ function mostrarSenha() {
         senha.type = "password";
     };
 }
-
-function criarConta() {
+function proximo() {
+    const tela = document.getElementById("inicio");
+    tela.classList.toggle('esquerda');
+}
+async function criarConta() {
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
     const usuario = document.getElementById("usuario").value;
     const criarConta = document.getElementById("criar-conta");
-
-    if (email === "" || senha === "" || usuario === "") {
+    const senhaHash = await gerarHash(email + senha);
+    
+    if (email === "" || senha === "") {
         alert("Por favor, preencha todos os campos.");
         return;
     };
@@ -43,7 +47,7 @@ function criarConta() {
 
     console.log("Conta criada!");
 
-    criarConta.style.display = "none";
+    criarConta.classList.toggle('esquerda');
     
 }
 function pegarConta() {
@@ -58,4 +62,22 @@ function pegarConta() {
 
     console.log("Email:", conta.email);
     console.log("Usuário:", conta.usuario);
+}
+async function gerarHash(texto) {
+    const dados = new TextEncoder().encode(texto);
+
+    const hash = await crypto.subtle.digest("SHA-256", dados);
+
+    const resultado = Array.from(new Uint8Array(hash))
+        .map(byte => byte.toString(16).padStart(2, "0"))
+        .join("");
+
+    return resultado;
+}
+async function testeHash() {
+    const texto = "email@email.comsenha123";
+
+    const hash = await gerarHash(texto);
+
+    console.log(hash);
 }
