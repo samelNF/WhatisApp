0aab9b515213b308cef882f574f0f3ca06021e1e
samelNF <nobrsamuel@gmail.com>
2026-09-20 12:19:30 -0300

    Atualiza cache da nova logo de grupo

--- script-base.js
+++ script-base.js
@@ -362,7 +362,7 @@ function aplicarAvatarGrupo(elemento, fotoUrl, corGrupo) {
         elemento.style.backgroundColor = "transparent";
         elemento.classList.remove("avatar-sem-foto");
     } else {
-        elemento.src = "svg/group-placeholder.svg?v=6c29b606";
+        elemento.src = "svg/group-placeholder.svg?v=d840e321";
         elemento.style.backgroundColor = cor;
         elemento.classList.add("avatar-sem-foto");
     }
