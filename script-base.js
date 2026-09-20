dcbc393ee5538f9d9fd669548e045572fb31e476
samelNF <nobrsamuel@gmail.com>
2026-09-20 13:09:06 -0300

    Restaura visual anterior do placeholder de grupo

--- script-base.js
+++ script-base.js
@@ -362,7 +362,7 @@ function aplicarAvatarGrupo(elemento, fotoUrl, corGrupo) {
         elemento.style.backgroundColor = "transparent";
         elemento.classList.remove("avatar-sem-foto");
     } else {
-        elemento.src = "svg/group-placeholder.svg?v=d406b3d2";
+        elemento.src = "svg/group-placeholder.svg?v=b92f3928";
         elemento.style.backgroundColor = cor;
         elemento.classList.add("avatar-sem-foto");
     }
